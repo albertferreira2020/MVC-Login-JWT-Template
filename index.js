12ff0fc3ba3fc35959eac3dbb6dc4e79d42f6693
Albert Ferreira <albert@espacow.com.br>
2020-11-06 01:29:52 -0300

    edit index

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 const compression = require('compression')
-const express = require('express'),app = express();
-
-const path = require('path');
-
+const express = require('express')
+const path = require('path')
 const Router = require('./src/routes/routes.route')
+
+const app = express()
 app.use(Router);
  
 app.use(function(req, res, next) {
@@ -12,12 +12,12 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
-  });
+});
   
   
-  app.use(compression());
-  app.disable('x-powered-by');
-  app.use(express.static(path.join(__dirname, 'src/views')));
+app.use(compression());
+app.disable('x-powered-by');
+app.use(express.static(path.join(__dirname, 'src/views')));
  
  app.listen(5000, function () {
     console.log('Server is running port 5000..');
